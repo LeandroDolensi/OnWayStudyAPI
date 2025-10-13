7db5fa64206c5af3062c9d7683b36d81af384c65
LeandroDolensi <leandro.dolensi@aluno.ifsp.edu.br>
2025-10-13 20:08:06 -0300

    feat: add app user

---
+++ apps/user/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+
+class User(models.Model):
+    nickname = models.CharField(max_length=100, unique=True)
+    senha = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.nickname
