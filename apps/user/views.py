498b88281c6607ea8f423cd2eefb6ece5bbb7f5f
LeandroDolensi <leandro.dolensi@aluno.ifsp.edu.br>
2025-10-27 08:56:43 -0300

    refactor(user): remove unused overload function

--- apps/user/views.py
+++ apps/user/views.py
@@ -8,8 +8,6 @@ from rest_framework.mixins import (
 from apps.user.models import User
 from apps.user.serializers import UserSerializer
 from security.authentication import OnWayStudyBaseAuthentication
-from security.permissions import IsOwner
-from rest_framework.permissions import AllowAny
 
 
 class UserViewSet(
@@ -23,14 +21,3 @@ class UserViewSet(
     serializer_class = UserSerializer
     lookup_field = "nickname"
     authentication_classes = [OnWayStudyBaseAuthentication]
-
-    def get_permissions(self):
-        """
-        The only method available to the public user is to create a new user.
-        In other words, authentication is not required to create a new user.
-        """
-        permission_classes = [AllowAny]
-        if self.action in ["retrieve", "update", "partial_update", "destroy"]:
-            permission_classes = [IsOwner]
-
-        return [permission() for permission in permission_classes]
