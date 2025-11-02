5ccd1ec462ef93b60603fab51930756b5e4fba17
LeandroDolensi <leandro.dolensi@aluno.ifsp.edu.br>
2025-11-02 09:15:46 -0300

    feat(course): add serializer

--- apps/course/serializers.py
+++ apps/course/serializers.py
@@ -1,9 +1,29 @@
-from rest_framework.serializers import ModelSerializer
+from rest_framework.serializers import SlugRelatedField
+from apps.institution.models import Institution
+from django_libs.custom_serializer import CustomModelSerializer
 from apps.course.models import Course
 
 
-class CourseSerializer(ModelSerializer):
+class CourseSerializer(CustomModelSerializer):
+    institution = SlugRelatedField(
+        slug_field="name", queryset=Institution.objects.all()
+    )
 
     class Meta:
         model = Course
-        fields = "__all__"
+        fields = [
+            "id",
+            "name",
+            "acronym",
+            "semesters",
+            "institution",
+            "created_at",
+            "updated_at",
+        ]
+        read_only_fields = ["created_at", "updated_at"]
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields["institution"].queryset = Institution.objects.filter(
+            user=self._get_user()
+        )
