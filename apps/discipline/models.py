65654b36ef6403fc640b1d7ce4308c6250a9196c
LeandroDolensi <leandro.dolensi@aluno.ifsp.edu.br>
2025-11-04 09:15:18 -0300

    feat: add more fields to discipline

--- apps/discipline/models.py
+++ apps/discipline/models.py
@@ -10,8 +10,20 @@ class Discipline(models.Model, PermissionBaseModel):
         RECOVERY = "RECOVERY", "Recovery"
         APPROVED = "APPROVED", "Approved"
 
+    class DisciplineStatusChoice(models.TextChoices):
+        IN_PROGRESS = "IN_PROGRESS", "In Progress"
+        COMPLETED = "COMPLETED", "Completed"
+        WAITING = "WAITING", "Waiting"
+        AWAITING_PREREQUISITE = "AWAITING_PREREQUISITE", "Awaiting Prerequisite"
+
     name = models.CharField(max_length=200)
     extra_information = models.TextField(blank=True, null=True)
+    semester = models.PositiveIntegerField(blank=True, null=True)
+    status = models.CharField(
+        max_length=30,
+        choices=DisciplineStatusChoice.choices,
+        default=DisciplineStatusChoice.WAITING,
+    )
     final_grade = models.DecimalField(
         max_digits=5, decimal_places=2, null=True, blank=True
     )
@@ -37,3 +49,27 @@ class Discipline(models.Model, PermissionBaseModel):
 
     def __str__(self):
         return f"{self.course.acronym}: {self.name}"
+
+
+class PrerequisiteDiscipline(models.Model):
+    main_discipline = models.ForeignKey(
+        Discipline, on_delete=models.CASCADE, related_name="prerequisites"
+    )
+    prerequisite = models.ForeignKey(
+        Discipline, on_delete=models.CASCADE, related_name="is_prerequisite_for"
+    )
+    created_at = models.DateTimeField(blank=True, default=get_timezone)
+    updated_at = models.DateTimeField(blank=True, null=True)
+
+    class Meta:
+        db_table = "prerequisite_discipline"
+        managed = True
+        constraints = [
+            models.UniqueConstraint(
+                fields=["main_discipline", "prerequisite"],
+                name="unique_discipline_prerequisite",
+            )
+        ]
+
+    def __str__(self):
+        return f"{self.prerequisite.name} -> {self.main_discipline.name}"
